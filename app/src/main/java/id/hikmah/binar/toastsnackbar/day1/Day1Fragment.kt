2b247354497637315af5cbdb8936379796f19f67
doyochi <nuzululhikmah117@gmail.com>
2022-03-28 21:48:00 +0700

    add create toast, custom color snackbar

--- app/src/main/java/id/hikmah/binar/toastsnackbar/day1/Day1Fragment.kt
+++ app/src/main/java/id/hikmah/binar/toastsnackbar/day1/Day1Fragment.kt
@@ -1,12 +1,15 @@
 package id.hikmah.binar.toastsnackbar.day1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import id.hikmah.binar.toastsnackbar.R
 import id.hikmah.binar.toastsnackbar.databinding.FragmentDay1Binding
 
 class Day1Fragment : Fragment() {
@@ -40,7 +43,8 @@ class Day1Fragment : Fragment() {
 
     private fun showToast(){
         binding.btnToast.setOnClickListener{
-            Toast.makeText(requireContext(), "Button Toast telah di tekan", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(requireContext(), "Button Toast telah di tekan", Toast.LENGTH_SHORT).show()
+            createToast("Button Toast telah di tekan").show()
         }
     }
 
@@ -54,9 +58,26 @@ class Day1Fragment : Fragment() {
         binding.btnActionsnack.setOnClickListener{
             Snackbar.make(it, "Button SnackBar dengan Action telah di tekan", Snackbar.LENGTH_INDEFINITE)
                 .setAction("Take Action"){
-                    Toast.makeText(requireContext(),"Take action di tekan!", Toast.LENGTH_LONG).show()
+//                    Toast.makeText(requireContext(),"Take action di tekan!", Toast.LENGTH_LONG).show()
+                    createToast("Take action di tekan").show()
                 }
                 .show()
         }
+    }
+
+    private fun createToast(message: String): Toast {
+        return Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT)
+    }
+
+    private fun createSnackBar(it: View, message: String): Snackbar {
+        return Snackbar.make(it, message, Snackbar.LENGTH_INDEFINITE)
+    }
+
+    private fun customColor(it: View, message: String) : Snackbar {
+        val snackbar = createSnackBar(it, message)
+        snackbar.setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.purple_200))
+            .setActionTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            .setTextColor(ContextCompat.getColor(requireContext(), R.color.teal_200))
+        return snackbar
     }
 }
